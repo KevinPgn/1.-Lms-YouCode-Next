1e1c323dc0970bdbf01a5416aa812c10ecb6c32b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-30 13:23:58 +0100

    card

--- src/features/account/components/CardAccountButton.tsx
+++ src/features/account/components/CardAccountButton.tsx
@@ -1,11 +1,18 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { LogOut } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export const CardAccountButton = () => {
+  const router = useRouter()
+
   return <div className="w-full flex flex-col gap-2 items-end">
-    <Button variant="outline" className="w-full hover:bg-zinc-800 bg-transparent dark:border-zinc-800">Settings</Button>
-    <Button variant="outline" className="w-full hover:bg-zinc-800 bg-transparent dark:border-zinc-800">Admin</Button>
+    <Button
+    onClick={() => router.push("/account/settings")}
+    variant="outline" className="w-full hover:bg-zinc-800 bg-transparent dark:border-zinc-800">Settings</Button>
+    <Button
+    onClick={() => router.push("/admin")}
+    variant="outline" className="w-full hover:bg-zinc-800 bg-transparent dark:border-zinc-800">Admin</Button>
     <Button variant="outline" className="w-fit px-2 mt-5 h-9 text-xs flex items-center gap-2 bg-transparent dark:border-zinc-800 dark:hover:bg-zinc-800">
       <LogOut className="w-3 h-3" />
       Logout
