f8a7677a782a8998f6f52f422748b0d0e9c22432
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-30 20:56:47 +0100

    SEO

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,6 +4,27 @@ import { StatsTotalCourses } from "@/features/adminManager/adminStats/components
 import { getSession } from "@/utils/CacheSession"
 import { redirect } from "next/navigation"
 import { getStatsTotal } from "@/features/adminManager/adminStats/server/CountStats"
+import { Suspense } from "react"
+import { absoluteUrl } from "@/utils/AbsoluteUrl"
+
+export const metadata = {
+  title: "Admin Stats",
+  description: "Admin Stats",
+  openGraph: {
+    title: "Admin Stats",
+    description: "Admin Stats",
+    type: "website",
+    url: absoluteUrl("/admin"),
+    siteName: "YouCode",
+  },
+  robots: {
+    index: true,
+    follow: true
+  },
+  alternates: {
+    canonical: absoluteUrl("/admin")
+  }
+}
 
 const AdminPage = async () => {
   const session = await getSession()
@@ -25,11 +46,23 @@ const AdminPage = async () => {
           <h2 className="text-3xl font-semibold tracking-tight mb-4 mt-5 border-b dark:border-zinc-800 border-zinc-200 w-fit pb-2">Courses</h2>
           <ButtonCourses />
         </div>
-
-        <StatsTotalCourses users={users} chapters={chapters} courses={courses} />
+        <Suspense fallback={<div>Loading...</div>}> 
+          <StatsContent />
+        </Suspense>
       </section>
     </div>
   )
 }
 
 export default AdminPage
+
+async function StatsContent(){
+  const result = await getStatsTotal({})
+  if (!result?.data) {
+    throw new Error("Failed to fetch stats")
+  }
+  
+  const {users, chapters, courses} = result.data
+
+  return <StatsTotalCourses users={users} chapters={chapters} courses={courses} />
+}
