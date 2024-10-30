836185bf1c975faa32e9c5a5c2d716cb4902de85
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-30 13:01:42 +0100

    user

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { CardAccount } from '@/features/account/components/CardAccount'
+import { getSession } from '@/utils/CacheSession'
+import { UserProps } from '@/lib/types'
 
-const AccountPage = () => {
+const AccountPage = async () => {
+  const session = await getSession()
+  const user = session?.user as UserProps
+  
   return (
-    <div>AccountPage</div>
+    <>
+        <CardAccount user={user} />    
+    </>
   )
 }
 
---
+++ src/features/account/components/CardAccount.tsx
@@ -0,0 +1,7 @@
+import { UserProps } from "@/lib/types";
+
+export const CardAccount = ({ user }: { user: UserProps }) => {
+  return <div className="w-[510px] mx-auto rounded-lg border border-zinc-800 bg-[#1D1916] mt-10 p-5">
+    
+  </div>
+}
