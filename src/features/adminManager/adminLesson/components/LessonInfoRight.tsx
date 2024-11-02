ac11b621941ba048babab5d6219b2a7063d71dd8
TeruBW <kevin.pigeon2810@gmail.com>
2024-11-02 14:05:44 +0100

    lessons

--- src/features/adminManager/adminLesson/components/LessonInfoRight.tsx
+++ src/features/adminManager/adminLesson/components/LessonInfoRight.tsx
@@ -6,11 +6,13 @@ import { Content } from "@tiptap/react"
 export const LessonInfoRight = ({ chapterContent }: { chapterContent: string }) => {
   const [value, setValue] = useState<Content>(chapterContent)
 
-  return <div className="rounded-2xl border dark:border-zinc-800 dark:bg-[#1D1916] bg-gray-50 shadow-lg p-6 flex-[3]">
+  return <div className="rounded-2xl border dark:border-zinc-800 dark:bg-[#1D1916] bg-gray-50 shadow-lg p-6 flex-[3] mb-3">
+    <h4 className="text-md font-semibold mb-5">Content</h4>
+   
    <MinimalTiptapEditor
       value={value}
       onChange={setValue}
-      className="w-full"
+      className="w-full max-h-[500px] overflow-y-auto"
       editorContentClassName="p-5"
       output="html"
       placeholder="Type your description here..."
