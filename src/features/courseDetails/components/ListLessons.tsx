22d254800437218ac35813b572d667781122bb80
TeruBW <kevin.pigeon2810@gmail.com>
2024-11-03 13:10:02 +0100

    enrolled

--- src/features/courseDetails/components/ListLessons.tsx
+++ src/features/courseDetails/components/ListLessons.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link"
 import { CircleDashed } from "lucide-react"
 
-export const ListLessons = ({lessons}: {lessons: any[]}) => {
+export const ListLessons = ({lessons, isEnrolled}: {lessons: any[], isEnrolled: boolean}) => {
   return <div className="flex flex-col gap-1">
     {lessons.map((lesson) => (
-      <Link href={`/course/${lesson.id}`} key={lesson.id} className="w-full h-9 px-5 flex items-center mt-2 border dark:border-zinc-800 rounded-md p-2 hover:bg-zinc-800/10 transition-colors">
-        <CircleDashed className="w-4 h-4 mr-2" />
-        <span className="text-sm font-medium">{lesson.title}</span>
-      </Link>
+      isEnrolled ? (
+        <Link href={`/course/${lesson.id}`} key={lesson.id} className="w-full h-9 px-5 flex items-center mt-2 border dark:border-zinc-800 rounded-md p-2 hover:bg-zinc-800/10 transition-colors">
+          <CircleDashed className="w-4 h-4 mr-2" />
+          <span className="text-sm font-medium">{lesson.title}</span>
+        </Link>
+      ) : (
+        <div key={lesson.id} className="w-full h-9 px-5 flex items-center mt-2 border dark:border-zinc-800 rounded-md p-2 opacity-50 cursor-not-allowed">
+          <CircleDashed className="w-4 h-4 mr-2" />
+          <span className="text-sm font-medium">{lesson.title}</span>
+        </div>
+      )
     ))}
   </div>
 }
--- src/features/explorerCourse/server/getAllCoursesPublished.ts
+++ src/features/explorerCourse/server/getAllCoursesPublished.ts
@@ -1,10 +1,18 @@
 "use server"
 import prisma from "@/lib/prisma";
 import { cache } from "react";
+import { getSession } from "@/utils/CacheSession";
 
 export const getAllCoursesPublished = cache(async () => {
+    const session = await getSession()
+    
     const courses = await prisma.course.findMany({
-        where: { published: true },
+        where: {
+            published: true,
+            authorId: {
+                not: session?.user?.id
+            },
+        },
         select: {
             id: true,
             title: true,
