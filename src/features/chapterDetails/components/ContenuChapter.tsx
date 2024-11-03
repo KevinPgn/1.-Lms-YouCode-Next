a1d2a37b45fdf352abf510462efbfd3ce3f703ad
TeruBW <kevin.pigeon2810@gmail.com>
2024-11-03 15:55:05 +0100

    chapters

--- src/features/chapterDetails/components/ContenuChapter.tsx
+++ src/features/chapterDetails/components/ContenuChapter.tsx
@@ -1,9 +1,12 @@
 export const ContenuChapter = ({chapterContent, chapterVideoUrl, chapterTitle}: {chapterContent: string, chapterVideoUrl: string, chapterTitle: string}) => {
   return <>
     <h2 className="mb-5">{chapterTitle}</h2>
-    <div
-    className="[&_h1]:text-3xl max-w-3xl mx-auto"
-    dangerouslySetInnerHTML={{__html: chapterContent}}
-    />
+    <div className="max-w-3xl mx-auto">
+        {chapterVideoUrl && <div className="w-full h-[500px] bg-zinc-800/10 rounded-md mb-5"></div>}
+        <div
+        className="prose prose-invert max-w-none [&_h1]:text-3xl"
+        dangerouslySetInnerHTML={{__html: chapterContent}}
+        />
+    </div>
   </>
 }
