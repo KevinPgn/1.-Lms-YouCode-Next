eb6339d0786b3dfd881d6d17bf7ed1d2d48dd341
TeruBW <kevin.pigeon2810@gmail.com>
2024-11-03 13:10:52 +0100

    join course

--- src/features/courseDetails/components/JoinCourseBtn.tsx
+++ src/features/courseDetails/components/JoinCourseBtn.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button"
+import { joinCourse } from "../server/joinCourse"
 
 export const JoinCourseBtn = ({courseId}: {courseId: string}) => {
-  return <Button className="w-fit px-5 mt-7 h-9 bg-[#E11D48] text-white hover:bg-[#E11D48]/80">Join</Button>
+  return <Button className="w-fit px-5 mt-7 h-9 bg-[#E11D48] text-white hover:bg-[#E11D48]/80" onClick={() => joinCourse({courseId})}>Join</Button>
 }
